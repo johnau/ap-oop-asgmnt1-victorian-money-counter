a4398da0c5de696c7a9d677fccb6887754c8ec66
John McSweeney <johnmcsweeney88@gmail.com>
2024-02-14 21:00:26 +0800

    comments + cleanup

--- VictorianMoneyCounter/Views/DenominationRow.xaml.cs
+++ VictorianMoneyCounter/Views/DenominationRow.xaml.cs
@@ -24,12 +24,16 @@ public partial class DenominationRow : UserControl, IViewModelBacked<Denominatio
         viewModel.RegisterSubscriberToQuantity((qty) => UpdateCoins(qty));
         //Loaded += DenominationRow_Loaded;
     }
+    public DenominationRowViewModel GetViewModel() => ViewModel;
 
+    /// <summary>
+    /// Adds or removes coins based to match the quantity provided
+    /// </summary>
+    /// <param name="quantity"></param>
     private void UpdateCoins(int quantity)
     {
         if (quantity > _coins.Count)
         {
-            // add coins
             for (int i = 0; i < quantity - _coins.Count; i++)
             {
                 var coin = SpawnCoin();
@@ -47,9 +51,6 @@ public partial class DenominationRow : UserControl, IViewModelBacked<Denominatio
             }
         }
     }
-
-    public DenominationRowViewModel GetViewModel() => ViewModel;
-
 
     /// <summary>
     /// Spawns a Coin above the canvas
@@ -79,45 +80,49 @@ public partial class DenominationRow : UserControl, IViewModelBacked<Denominatio
     {
         Random random = new();
 
+        // configuration variables
         var animationDuration = 1d; // 1 second
-        var fallSpeed = 0.1; // 0.1 second
-        var dir = random.Next(0, 2) == 0 ? 1 : -1;
+        var fallSpeed = 0.1;        // 0.1 second
+        var bounceForce = image.Height * 3; // force of bounce
+        var horizontalForce = random.Next(0, (int)image.Width / 2); // sideways force
+
+        var direction = random.Next(0, 2) == 0 ? 1 : -1;
         var startX = (RowCanvas.ActualWidth - image.Width) / 2;
         var startY = -image.Height;
-        var finalY = RowCanvas.ActualHeight - image.Height;
+        var endY = RowCanvas.ActualHeight - image.Height;
 
         var animationY = new DoubleAnimationUsingKeyFrames();
         var animationX = new DoubleAnimationUsingKeyFrames();
 
-        // define the fall
+        // manually define the fall - start of fall
         animationY.KeyFrames.Add(new LinearDoubleKeyFrame(startY, TimeSpan.FromSeconds(0)));
-        animationY.KeyFrames.Add(new LinearDoubleKeyFrame(finalY, TimeSpan.FromSeconds(fallSpeed)));
-
         animationX.KeyFrames.Add(new LinearDoubleKeyFrame(startX, TimeSpan.FromSeconds(0)));
+
+        // end of fall
+        animationY.KeyFrames.Add(new LinearDoubleKeyFrame(endY, TimeSpan.FromSeconds(fallSpeed)));
         animationX.KeyFrames.Add(new LinearDoubleKeyFrame(startX, TimeSpan.FromSeconds(fallSpeed)));
 
         // define the bounces
-        var maxHeight = image.Height * 3;
-        var horizontalForce = random.Next(0, (int)image.Width / 2);
-        var x = startX;
+        var locationX = startX;
         for (double t = fallSpeed; t <= animationDuration; t += 0.05)
         {
-            var bounceHeight = MathHelpers.CalculateBounceHeight(animationDuration, t, maxHeight); // was (finalY + image.Height)
-            var bounceLength = MathHelpers.CalculateBounceLength(animationDuration, t, horizontalForce);
             var keyTime = TimeSpan.FromSeconds(t);
-            animationY.KeyFrames.Add(new LinearDoubleKeyFrame(finalY - bounceHeight, keyTime));
-            x += bounceLength * dir;
-            animationX.KeyFrames.Add(new LinearDoubleKeyFrame(x, keyTime));
+            var bounceHeight = MathHelpers.CalculateBounceHeight(animationDuration, t, bounceForce);
+            var bounceLength = MathHelpers.CalculateBounceLength(animationDuration, t, horizontalForce);
+                        
+            locationX += bounceLength * direction;
+            animationX.KeyFrames.Add(new LinearDoubleKeyFrame(locationX, keyTime));
+            animationY.KeyFrames.Add(new LinearDoubleKeyFrame(endY - bounceHeight, keyTime));
         }
 
-        Storyboard.SetTarget(animationY, image);
-        Storyboard.SetTargetProperty(animationY, new PropertyPath(Canvas.TopProperty));
         Storyboard.SetTarget(animationX, image);
         Storyboard.SetTargetProperty(animationX, new PropertyPath(Canvas.LeftProperty));
+        Storyboard.SetTarget(animationY, image);
+        Storyboard.SetTargetProperty(animationY, new PropertyPath(Canvas.TopProperty));
 
         var storyboard = new Storyboard();
-        storyboard.Children.Add(animationY);
         storyboard.Children.Add(animationX);
+        storyboard.Children.Add(animationY);
         storyboard.Begin();
     }
 

cc23a0ac79680c29f044a29827d955ae003423d4
John McSweeney <johnmcsweeney88@gmail.com>
2024-02-16 18:08:31 +0800

    comments

--- VictorianMoneyCounter/StartupHelpers/ServiceExtensions.cs
+++ VictorianMoneyCounter/StartupHelpers/ServiceExtensions.cs
@@ -7,8 +7,8 @@ public static class ServiceExtensions
     public static void AddWpfComponentFactory<T>(this IServiceCollection services)
         where T : class
     {
-        services.AddTransient<T>();
-        services.AddSingleton<Func<T>>(x => () => x.GetService<T>()!); // Service Locator function for AbstractFactory
-        services.AddSingleton<IAbstractFactory<T>, AbstractFactory<T>>();
+        services.AddTransient<T>(); // Register the type reference
+        services.AddSingleton<Func<T>>(x => () => x.GetService<T>()!); // Register IServiceProvider service locator function for AbstractFactory
+        services.AddSingleton<IAbstractFactory<T>, AbstractFactory<T>>(); // Register the factory (injected with IServiceProvider service locator func
     }
 }
